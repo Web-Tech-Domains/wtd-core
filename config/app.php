e3a4528868efc18ba6b2575c2bafb2f3c939dc95
Souravlai7 <75032678+Souravlai7@users.noreply.github.com>
2026-07-11 12:40:18 -0500

    update

--- config/app.php
+++ config/app.php
@@ -2,10 +2,12 @@
 
 declare(strict_types=1);
 
+$environment = env('APP_ENV', 'development');
+
 return [
     'name' => env('APP_NAME', 'WTD Core'),
-    'env' => env('APP_ENV', 'production'),
-    'debug' => env('APP_DEBUG', false),
+    'env' => $environment,
+    'debug' => env('APP_DEBUG', $environment !== 'production'),
     'key' => env('APP_KEY', 'wtd-core'),
     'url' => env('APP_URL', 'http://localhost'),
     'providers' => [],
--- config/developer.php
+++ config/developer.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
+$environment = env('APP_ENV', 'development');
+
 return [
-    'enabled' => env('APP_ENV', 'production') !== 'production',
+    'enabled' => $environment !== 'production',
     'debug_toolbar' => env('WTD_DEBUG_TOOLBAR', false),
     'api_docs' => env('WTD_API_DOCS', false),
     'error_pages' => true,
